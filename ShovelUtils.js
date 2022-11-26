738d2b62b2fad7ecf6898afcdf89e027417a8eeb
TheShovel <niccata24@gmail.com>
2022-11-26 21:41:42 +0200

    heehee

--- ShovelUtils.js
+++ ShovelUtils.js
@@ -1,15 +1,11 @@
 (function(Scratch) {
   const vm = Scratch.vm;
   'use strict';
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/beta.js")
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/utilities.js")
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/CloudLinkWS.js")
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/files.js")
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/runtime-options.js")
-vm.extensionManager.loadExtensionURL("https://theshovel.github.io/")
-vm.extensionManager.loadExtensionURL("")
-
-
+vm.extensionManager.loadExtensionURL("http://localhost:8000/beta.js")
+vm.extensionManager.loadExtensionURL("http://localhost:8000/utilities.js")
+vm.extensionManager.loadExtensionURL("http://localhost:8000/CloudLinkWS.js")
+vm.extensionManager.loadExtensionURL("http://localhost:8000/files.js")
+vm.extensionManager.loadExtensionURL("http://localhost:8000/runtime-options.js")
   class ShovelUtils {
     getInfo () {
       return { 
